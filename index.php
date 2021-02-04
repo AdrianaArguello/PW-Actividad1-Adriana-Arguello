c86003807e55ae1c3de50050bb392879ecfb938b
marguello <mariana.arguello@urbe.edu>
2021-02-03 20:30:37 -0400

    menu-opciones

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Opciones</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="flex_container">
+        <div class="container">
+            <h1>Proyectos Adriana Arguello, c.i: 29.509.496</h1>
+                <a class="item-a" href="./teorema-pitagoras/index.php">Actividad 1</a>
+                <a class="item-a" href="./octogono-regular/index.php">Actividad 2</a>
+                <a class="item-a" href="./arreglo-asociativo/index.html">Actividad 3</a>
+        </div>
+    </div>
+</body>
+</html>
